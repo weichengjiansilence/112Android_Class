d82f320cf6275c0c3f3bb38df40bb0e424d2522d
weichengjiansilence <weichengjian930201@gmail.com>
2024-03-21 22:22:40 +0800

    2024.3.7

---
+++ UI_Test2/app/src/main/java/com/example/ui_test2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.ui_test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+    public void sendString(View view){
+        EditText edtName = (EditText)  findViewById(R.id.editName);
+        String string2Send= edtName.getText().toString();
+
+        Intent intent = new Intent(this, DisplayStringActivity.class);
+        intent.putExtra("username",string2Send);
+        startActivity(intent);
+    }
+}
