52efb3154c73e5d644824ea41bdba95e4adae5de
weichengjiansilence <weichengjian930201@gmail.com>
2024-05-05 16:17:13 +0800

    2024.5.2

--- CheckBoxDemo/app/src/main/java/com/example/checkboxdemo/MainActivity.java
+++ CheckBoxDemo/app/src/main/java/com/example/checkboxdemo/MainActivity.java
@@ -11,8 +11,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
-    private CheckBox original, beef, seafood;
-    private int[] chkIDs = {R.id.chkOriginal, R.id.chkBeef, R.id.chkSeafood};
+    private  CheckBox original,beef,seafood;
+    private int[] chkIDs = {R.id.chkOriginal, R.id.chkBeef,R.id.chkSeafood};
     private TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +21,23 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
 
         original = (CheckBox) findViewById(R.id.chkOriginal);
         beef = (CheckBox) findViewById(R.id.chkBeef);
-        seafood = (CheckBox) findViewById(R.id.chkSeafood);
+        seafood =(CheckBox) findViewById(R.id.chkSeafood);
 
-        for(int id : chkIDs){
+        for ( int id : chkIDs){
             CheckBox chk = (CheckBox) findViewById(id);
             chk.setOnCheckedChangeListener(this);
         }
         output = (TextView) findViewById(R.id.lblOutput);
-        Button btnComfirm = (Button) findViewById(R.id.btnconfirm);
-        btnComfirm.setOnClickListener(new View.OnClickListener() {
+        Button btnConfirm =(Button) findViewById(R.id.btnconfirm);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String str = "";
-                if(original.isChecked())
+                if (original.isChecked())
                     str += original.getText() + "\n";
-                if(beef.isChecked())
+                if (beef.isChecked())
                     str += beef.getText() + "\n";
-                if(seafood.isChecked())
+                if (seafood.isChecked())
                     str += seafood.getText() + "\n";
 
                 TextView output = (TextView) findViewById(R.id.lblOutput);
@@ -47,26 +47,24 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
     }
 
     @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        TextView txvStatus = (TextView) findViewById(R.id.txvStatus);
+    public void onCheckedChanged(CompoundButton buttonView,boolean isChecked){
+        //TextView output = (TextView) findViewById(R.id.lblOutput);
+        TextView txvStatus= (TextView) findViewById(R.id.txvStatus);
         int id = buttonView.getId();
         String str = "";
         String status = "";
-
-        if(id == R.id.chkOriginal){
-            //output.setText("你點選的是原味...\n");
-            status = (isChecked? "選取" : "取消") + "原味";
-        }else if(id == R.id.chkBeef){
-            //output.setText("你點選的是牛肉...\n");
-            status = (isChecked? "選取" : "取消") + "牛肉";
-        }else if(id == R.id.chkSeafood){
-            //output.setText("你點選的海鮮...\n");
-            status = (isChecked? "選取" : "取消") + "海鮮";
+        if (id == R.id.chkOriginal){
+            status = (isChecked? "選取" : "取消" ) + "原味...";
+        }else if (id == R.id.chkBeef){
+            status = (isChecked? "選取" : "取消" ) + "牛肉...";
+        }else if (id == R.id.chkSeafood){
+            status = (isChecked? "選取" : "取消" ) + "海鮮...";
         }
         txvStatus.setText(status);
-        for(int i : chkIDs){
-            CheckBox chk = (CheckBox) findViewById(id);
-            if(chk.isChecked())
+
+        for (int i :chkIDs){
+            CheckBox chk = (CheckBox) findViewById(i);
+            if (chk.isChecked())
                 str += chk.getText() + "\n";
         }
         output.setText(str);
